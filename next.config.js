9aea21b521216b232900f05a698311e7e5a6cc9f
Pelayo AI <pelayo@mycompi.com>
2026-05-11 17:47:58 +0200

    fix: correct next.config.js - serverExternalPackages inside experimental

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pg'],
+  experimental: {
+    serverExternalPackages: ['pg'],
+  },
 }
 
 module.exports = nextConfig
